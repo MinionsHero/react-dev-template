2103abfed3fb56c7998f3d627fade6540e84e027
MinionsHero <vijonhero@gmail.com>
2019-12-25 15:08:13 +0800

    fix windows can not start bugs

--- build/config/dev.config.js
+++ build/config/dev.config.js
@@ -1,19 +1,22 @@
-const path = require('path')
-const getConfig = require('../utils/getConfig')
-const publicPath = path.resolve(__dirname, '../../public')
-const distPath = path.resolve(__dirname, '../../dist')
+const path = require('path');
+const getConfig = require('../utils/getConfig');
+const publicPath = path.resolve(__dirname, '../../public');
+const distPath = path.resolve(__dirname, '../../dist');
+
 module.exports = {
-  quiet: true,// 开启FriendlyErrorsPlugin必须设置这个为true
-  contentBase: distPath, //服务基于文件夹
-  host: '0.0.0.0',// 允许外部ip访问
-  useLocalIp: true,// 允许本地ip访问
-  hot: true,//开启热加载,必须同时使用webpack.HotModuleReplacementPlugin
-  open: 'Google Chrome',//启动后打开Chrome
-  overlay: false,//不在页面显示错误
-  port: 9000,//端口号
-  publicPath: publicPath, //可以通过地址根路径访问的静态文件夹
-  proxy: getConfig('apiproxy', {}),//代理
+  quiet: true, // 开启FriendlyErrorsPlugin必须设置这个为true
+  contentBase: [
+    distPath,
+    publicPath
+  ], //服务基于静态文件夹
+  host: '0.0.0.0', // 允许外部ip访问
+  useLocalIp: true, // 允许本地ip访问
+  hot: true, //开启热加载,必须同时使用webpack.HotModuleReplacementPlugin
+  // open: process.platform == 'win32' ? 'Chrome' : 'Google Chrome', //启动后打开Chrome
+  overlay: false, //不在页面显示错误
+  port: 9000, //端口号
+  proxy: getConfig('apiproxy', {}), //代理
   writeToDisk: (filePath) => {
     return true;
-  },//是否写入硬盘
-}
+  }, //是否写入硬盘
+};
--- build/dev.js
+++ build/dev.js
@@ -1,9 +1,21 @@
-const webpack = require('webpack')
-const devServer = require('webpack-dev-server')
-const webpackConfig = require('./config/webpack.config')
-const devConfig = require('./config/dev.config')
+const webpack = require('webpack');
+const DevServer = require('webpack-dev-server');
+const webpackConfig = require('./config/webpack.config');
+const devConfig = require('./config/dev.config');
 
-// devServer卸载webpack配置中只能在watch模式起作用,但不能自动启动devServer,
-// 因此必须传给webpackDevServer
-const server = new devServer(webpack(webpackConfig), devConfig)
-server.listen(devConfig.port)
+const portfinder = require('portfinder');
+const open = require('open');
+
+portfinder.getPort({
+  port: devConfig.port,
+}, function (err, port) {
+  if (err) {
+    return console.error(err);
+  }
+  // devServer卸载webpack配置中只能在watch模式起作用,但不能自动启动devServer,
+  // 因此必须调用webpackDevServer.listens
+  const server = new DevServer(webpack(webpackConfig), devConfig);
+  server.listen(port, 'localhost', function () {
+    open('http://localhost:' + port);
+  });
+});
